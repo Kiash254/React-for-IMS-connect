b6e8fc2bea1b320f8dcf51bba3364b8a8f9d054e
roh1t-coder <rohitcherool@gmail.com>
2024-12-26 19:56:16 +0300

    added new method to allow code have a live api

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:8000//api/',
+  baseURL: 'https://melodic-naiad-464d1e.netlify.app/',
   headers: {
     'Content-Type': 'application/json',
   },
